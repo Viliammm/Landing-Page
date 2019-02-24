aeeca0a49d4a55475e841a49832ee3dd126e375f
Viliammm <38937143+Viliammm@users.noreply.github.com>
2019-02-24 21:51:36 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
 /* --- Language Selection Hiding --- */
+// $(".lang").on("mouseenter", function(){
+// 	$(".lang-hidden").removeClass("hide")
+// });
+
+// $(".lang").on("mouseleave", function(){
+// 	$(".lang-hidden").addClass("hide")
+// });
+
+//  --- Navbar Selection Hiding --- 
+
+// $(".nav").on("mouseenter", function(){
+// 	$(".nav-hidden").removeClass("hide")
+// });
+
+// $(".nav").on("mouseleave", function(){
+// 	$(".nav-hidden").addClass("hide")
+// });
+
+
+
+
+// $("body").on("click", function(){
+// 	$(".lang-hidden").addClass("hide")
+// });
+
 $(".lang").on("mouseenter", function(){
 	$(".lang-hidden").removeClass("hide")
 });
 
-$(".lang").on("mouseleave", function(){
-	$(".lang-hidden").addClass("hide")
-});
-
-/* --- Navbar Selection Hiding --- */
-
-$(".nav").on("mouseenter", function(){
-	$(".nav-hidden").removeClass("hide")
-});
-
-$(".nav").on("mouseleave", function(){
-	$(".nav-hidden").addClass("hide")
-});
-
-
-
-
 $("body").on("click", function(){
-	$(".lang-hidden").addClass("hide");
-	$(".nav-hidden").addClass("hide");
-	
+	$(".lang-hidden").addClass("hide")
 });
