800ebc76c80ff930f7342f5fef389fb9ab6d409f
Viliammm <38937143+Viliammm@users.noreply.github.com>
2019-02-25 00:13:04 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,19 +24,19 @@ $(".container").on("mouseenter", function(){
 
 
 $("#nav-who-am-i").click(function() {
-    $('html, body').animate({ 
+    $('html, body, .container').animate({ 
     	scrollTop: $("#who-am-i").offset().top
     }, 1000);
 });
 
 $("#nav-my-work").click(function() {
-    $('html, body').animate({ 
+    $('html, body, .container').animate({ 
     	scrollTop: $("#my-work").offset().top
     }, 1000);
 });
 
 $("#nav-contact").click(function() {
-    $('html, body').animate({ 
+    $('html, body, .container').animate({ 
     	scrollTop: $("#contact").offset().top
     }, 1000);
 });
