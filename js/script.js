05e5d509be5b514b05b83ba01e6a0bf1fd2ff7e0
Viliammm <38937143+Viliammm@users.noreply.github.com>
2019-02-24 23:42:30 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,8 +23,21 @@ $(".container").on("mouseenter", function(){
 /* SLIDE */
 
 
+$("#nav-who-am-i").click(function() {
+    $('html, body').animate({ 
+    	scrollTop: $("#who-am-i").offset().top
+    }, 1000);
+});
+
+$("#nav-my-work").click(function() {
+    $('html, body').animate({ 
+    	scrollTop: $("#my-work").offset().top
+    }, 1000);
+});
+
 $("#nav-contact").click(function() {
     $('html, body').animate({ 
     	scrollTop: $("#contact").offset().top
     }, 1000);
 });
+
