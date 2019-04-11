1ee35d6fa4f7663b3c407abb539e0bef089c3e44
Viliammm <38937143+Viliammm@users.noreply.github.com>
2019-04-11 23:35:26 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // NAV SLIDE
 
-function arrowUp(){
-	$(".arrow").text("▲")
-};
-
 function arrowDown(){
 	$(".arrow").text("▼")
+};
+
+function arrowUp(){
+	$(".arrow").text("▲")
 };
 
 function navSlideDown(){
@@ -32,44 +32,44 @@ function navSlide(){
 		$(this).removeClass();
 		$(this).addClass("nav-show");
 	});
-}
+};
 
 navSlide();
 
-
 // PAGE SLIDE
 
+function scrollDown(){
+	$("#nav-who-am-i").click(function() {
+	    $('html, body').animate({ 
+	    	scrollTop: $("#who-am-i").offset().top
+	    }, 750);
+	});
+	$("#nav-my-work").click(function() {
+	    $('html, body').animate({ 
+	    	scrollTop: $("#my-work").offset().top
+	    }, 750);
+	});
+	$("#nav-contact").click(function() {
+	    $('html, body').animate({ 
+	    	scrollTop: $("#contact").offset().top
+	    }, 750);
+	});
+};
 
-$("#nav-who-am-i").click(function() {
-    $('html, body').animate({ 
-    	scrollTop: $("#who-am-i").offset().top
-    }, 750);
-});
+function scrollUp(){
+	$("#back-to-top").click(function() {
+		$('html,body').animate({
+			scrollTop: 0}, 750);
+	});
+};
 
-$("#nav-my-work").click(function() {
-    $('html, body').animate({ 
-    	scrollTop: $("#my-work").offset().top
-    }, 750);
-});
-
-$("#nav-contact").click(function() {
-    $('html, body').animate({ 
-    	scrollTop: $("#contact").offset().top
-    }, 750);
-});
-
-$("#back-to-top").click(function() {
-	$('html,body').animate({
-		scrollTop: 0}, 750);
-});
-
-
-
-
-
-
+scrollDown();
+scrollUp();
 
 // STARS FADEIN
 
+function starsFadeIn(){
+	$(".background").fadeIn(10000);
+};
 
-$(".background").fadeIn(10000);
+starsFadeIn();
